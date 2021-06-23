52bff73f0bf6d2d099c053deb28971e3a18b3cdb
Akash Sharma <35839624+Adaverse@users.noreply.github.com>
2021-06-23 18:01:27 +0530

    Update url.js

--- src/services/url.js
+++ src/services/url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://stock-market-charting-java.herokuapp.com/';
+export const BASE_URL = 'https://stock-market-charting-java.herokuapp.com';
 // export const BASE_URL = 'http://localhost:8080';
 // checking...
